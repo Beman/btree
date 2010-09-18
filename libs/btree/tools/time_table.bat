569a338468423db3b4236147fa2b98011b862d94
Beman <bdawes@acm.org>
2010-09-18 16:09:07 -0400

    Initial commit

---
+++ libs/btree/tools/time_table.bat
@@ -0,0 +1,17 @@
+bjam --toolset=gcc-4.5
+echo "GCC/MinGW 4.5" >time_table.html
+bin/bt_time 5000000 -stl -p4096 -c32 -native -html 2>>time_table.html
+type time_table.html
+bin/bt_time 5000000 -stl -p4096 -c1000 -native -html 2>>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c10000 -native -html 2>>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c10000 -big -html 2>>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c10000 -little -html 2>>time_table.html
+bin/bjam --toolset=msvc-10.0express
+echo "VC++ 10.0 Express" >>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c32 -native -html 2>>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c1000 -native -html 2>>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c10000 -native -html 2>>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c10000 -big -html 2>>time_table.html
+bin/bt_time 5000000 -stl -p4096 -c10000 -little -html 2>>time_table.html
+type time_table.html
+
