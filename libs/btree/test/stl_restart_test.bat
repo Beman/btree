9af065552827489b38927efcf44fdf7ad8cd9950
Beman <bdawes@acm.org>
2010-10-12 08:01:47 -0400

    Test program renamed

--- libs/btree/test/stl_restart_test.bat
+++ libs/btree/test/stl_restart_test.bat
@@ -1,5 +1,5 @@
-stl_equivalence_test -max=10 -min=5 -cycles=3 -dump=2 -v
+stl_test -max=10 -min=5 -cycles=3 -dump=2 -v
 copy /y restart.* restart2.*
-stl_equivalence_test -max=10 -min=5 -cycles=1 -dump=-1 -v
-stl_equivalence_test -restart -cycles=1 -v
-bc3 stl_equivalence.btr restart2.btr
+stl_test -max=10 -min=5 -cycles=1 -dump=-1 -v
+stl_test -restart -cycles=1 -v
+bc3 stl.btr restart2.btr
