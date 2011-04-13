1ec64f36354809db8a3ac6d6e26015c31725f99f
Beman <bdawes@acm.org>
2011-04-13 08:05:05 -0400

    Add support directory with strbuf.hpp initial commit

---
+++ boost/btree/support/strbuf.hpp
@@ -0,0 +1,23 @@
+//  boost/btree/support/strbuf.hpp  ----------------------------------------------------//
+
+//  Copyright Beman Dawes 2011
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  http://www.boost.org/LICENSE_1_0.txt
+
+#include <string>
+#include <cstring>
+#include <boost/assert.hpp>
+
+#ifndef BOOST_STRBUF_HPP
+#define BOOST_STRBUF_HPP
+
+namespace boost
+{
+namespace btree
+{
+
+}
+}
+
+#endif  // BOOST_STRBUF_HPP
