d5de38c2549794b36ed98841107d557fabf90e26
Beman <bdawes@acm.org>
2011-05-16 14:26:52 -0400

    Tweaks

--- libs/btree/tools/install.bat
+++ libs/btree/tools/install.bat
@@ -1,4 +1,4 @@
 rem Copyright Beman Dawes 2011
 rem Distributed under the Boost Software License, Version 1.0.  See http://www.boost.org/LICENSE_1_0.txt 
 git clone git://github.com/Beman/Boost-Btree.git btree
-svn export --force %BOOST_TRUNK% btree
+svn export --force http://svn.boost.org/svn/boost/trunk btree
