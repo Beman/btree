998fed38a6aa4e2a2c83414011488d03b9e08937
Beman <bdawes@acm.org>
2013-08-02 08:51:27 -0400

    Describe possible design strategy for dealing with maps having keys living in the btree vs maps having keys living in the flat file.

---
+++ include/boost/btree/index_map.hpp
@@ -0,0 +1,6 @@
+Consider four map classes:
+
+index_map                   // Key lives in btree, T lives in flat file
+index_multimap              // Key lives in btree, T lives in flat file
+index_indirect_map          // Both Key and T live in flat file
+index_indirect_multimap     // Both Key and T live in flat file
