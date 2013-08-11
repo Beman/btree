b2a8366d085bce67ac83f29c96101aad489b5f57
Beman <bdawes@acm.org>
2013-08-11 11:39:16 -0400

    Initial commit; untested.

---
+++ example/set_example2.cpp
@@ -0,0 +1,29 @@
+//  set_example2.cpp  ------------------------------------------------------------------//
+
+//  Copyright Beman Dawes 2013
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+
+#include <utility>
+#include <boost/btree/index_set.hpp>
+#include <iostream>
+using std::cout;
+
+int main()
+{
+  typedef boost::btree::btree_set<boost::string_view> set_type;
+
+  set_type ndx_set("set_example2.ndx", 1000, boost::btree::flags::truncate);
+
+  ndx_set.insert("eat");
+  ndx_set.insert("drink");
+  ndx_set.ensert("be merry");
+
+  cout << "ndx_set:\n";
+  for (set_type::iterator it = ndx_set.begin(); it != ndx_set.end(); ++it)
+    cout << "  \"" << *it << "\"\n";
+
+  return 0;
+}
